80378558b3e4179eb0fd162f5f9c4f18cd94a0a7
kuwapica <annidaaiska@gmail.com>
2025-11-25 21:11:39 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tokokita/ui/registrasi_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Toko Kita Shinsuke', home: RegistrasiPage());
+  }
+}
